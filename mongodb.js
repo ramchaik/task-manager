84f378d13f6e658f75314c42ce2685a40178b19a
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2019-09-02 17:33:17 +0530

    connected to mongodb

--- mongodb.js
+++ mongodb.js
@@ -1,2 +1,23 @@
 // CRUD create read update delete
 const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+
+const connectionURL = 'mongodb://127.0.0.1:27017';
+const databaseName = 'task-manager';
+
+MongoClient.connect(
+  connectionURL,
+  { useNewUrlParser: true },
+  (error, client) => {
+    if (error) {
+      return 'Unable to connect to the database!';
+    }
+
+    const db = client.db(databaseName);
+
+    db.collection('users').insertOne({
+      name: 'Vaibhav',
+      age: 23
+    });
+  }
+);
