ee08f087d56702d94538fe8973d3325095382688
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2019-09-11 09:00:46 +0530

    add validation and sanitization using mongoose and validator

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
   useNewUrlParser: true,
@@ -7,13 +8,41 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 
 const User = mongoose.model('User', {
   name: {
-    type: String
+    type: String,
+    require: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate( value ) {
+      if (!validator.isEmail(value)) {
+        throw new Error('Email is invalid.');
+      }
+    }
   },
   age: {
-    type: Number
+    type: Number,
+    default: 0,
+    validate(value) {
+      if ( value < 0 ) {
+        throw new Error('Age must be a positive number.');
+      }
+    }
   }
 });
 
+const Task = mongoose.model('Task', {
+  description: {
+    type: String
+  },
+  completed: {
+    type: Boolean
+  }
+});
+/* 
 const Ram = new User({
   name: 'Ram',
   age: 23
@@ -25,4 +54,14 @@ Ram.save()
   })
   .catch(err => {
     console.log(err);
-  });
+  }); */
+const Ram = new User({
+  name: "  Ram   ",
+  email: "  aSSf@gma.com "
+});
+
+Ram.save().then(res => {
+  console.log(res);
+}).catch(err => {
+  console.log(err);
+});
