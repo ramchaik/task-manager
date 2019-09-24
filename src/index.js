a53552165ce9c7ce6457abd38ebd5153d335b8d3
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2019-09-24 23:44:18 +0530

    segregate route to smaller seprate router on resource

--- src/index.js
+++ src/index.js
@@ -2,154 +2,15 @@ const express = require('express');
 require('./db/mongoose');
 const User = require('./models/user');
 const Task = require('./models/task');
+const userRouter = require('./routers/users');
+const taskRouter = require('./routers/tasks');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-
-app.post('/users', async (req, res) => {
-  try {
-    const user = new User(req.body);
-    await user.save();
-
-    res.status(201).send(user);
-  } catch (e) {
-    res.status(400).send(e);
-  }
-});
-
-app.post('/tasks', async (req, res) => {
-  try {
-    const task = new Task(req.body);
-    await task.save();
-    res.status(201).send(task);
-  } catch (e) {
-    res.status(400).send(e);
-  }
-});
-
-app.get('/users', async (req, res) => {
-  try {
-    const users = await User.find({});
-
-    res.send(users);
-  } catch (e) {
-    res.status(500).send(e);
-  }
-});
-
-app.get('/users/:id', async (req, res) => {
-  try {
-    const _id = req.params.id;
-    const user = User.findById(_id);
-    if (!user) {
-      return res.status(404);
-    }
-    res.send(user);
-  } catch (e) {
-    res.status(500).send(e);
-  }
-});
-
-app.get('/tasks', async (req, res) => {
-  try {
-    const tasks = await Task.find({});
-    res.send(tasks);
-  } catch (e) {
-    res.status(500).send(e);
-  }
-});
-
-app.get('/tasks/:id', async (req, res) => {
-  try {
-    const _id = req.params.id;
-    const task = await Task.findById(_id);
-    if (!task) {
-      return res.status(404);
-    }
-    res.send(task);
-  } catch (e) {
-    res.status(500).send(e);
-  }
-});
-
-app.patch('/users/:id', async (req, res) => {
-  const updates = Object.keys(req.body);
-  const allowedUpdates = ['name', 'email', 'password', 'age'];
-  const isValidUpdate = updates.every(update =>
-    allowedUpdates.includes(update)
-  );
-  if (!isValidUpdate) {
-    return res.status(400).send({ error: 'Invalid updates!' });
-  }
-
-  try {
-    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true
-    });
-
-    if (!user) {
-      return res.status(404).send();
-    }
-    res.send(user);
-  } catch (e) {
-    res.status(400).send(e);
-  }
-});
-
-app.patch('/tasks/:id', async (req, res) => {
-  const updates = Object.keys(req.body);
-  const allowedUpdates = ['description', 'completed'];
-  const isValidUpdate = updates.every(update =>
-    allowedUpdates.includes(update)
-  );
-
-  if (!isValidUpdate) {
-    return res.status(400).send({ error: 'Invalid updates!' });
-  }
-
-  try {
-    const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true
-    });
-
-    if (!task) {
-      return res.status(404).send();
-    }
-    res.send(task);
-  } catch (e) {
-    res.status(400).send(e);
-  }
-});
-
-app.delete('/users/:id', async (req, res) => {
-  try {
-    const user = await User.findByIdAndDelete(req.params.id);
-
-    if (!user) {
-      return res.status(404).send();
-    }
-    res.send(user);
-  } catch (e) {
-    res.status(500).send(e);
-  }
-});
-
-app.delete('/tasks/:id', async (req, res) => {
-  try {
-    const task = await Task.findByIdAndDelete(req.params.id);
-
-    if (!task) {
-      return res.status(404).send();
-    }
-    res.send(task);
-  } catch (e) {
-    res.status(500).send(e);
-  }
-});
+app.use(userRouter);
+app.use(taskRouter);
 
 app.listen(port, () => {
   console.log('Server is up at ' + port + '...');
---
+++ src/routers/users.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const User = require('../models/user');
+const router = new express.Router();
+
+router.post('/users', async (req, res) => {
+  try {
+    const user = new User(req.body);
+    await user.save();
+
+    res.status(201).send(user);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+router.get('/users', async (req, res) => {
+  try {
+    const users = await User.find({});
+
+    res.send(users);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
+
+router.get('/users/:id', async (req, res) => {
+  try {
+    const _id = req.params.id;
+    const user = await User.findById(_id);
+    if (!user) {
+      return res.status(404);
+    }
+    res.send(user);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
+
+router.patch('/users/:id', async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ['name', 'email', 'password', 'age'];
+  const isValidUpdate = updates.every(update =>
+    allowedUpdates.includes(update)
+  );
+  if (!isValidUpdate) {
+    return res.status(400).send({ error: 'Invalid updates!' });
+  }
+
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    });
+
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.send(user);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+router.delete('/users/:id', async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.send(user);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
+
+module.exports = router;
