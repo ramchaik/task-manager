3b3c4aef5391c7833de6c174585dccbb82ca8b32
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2019-09-27 00:08:27 +0530

    added mongoose middleware for hashing password(bcrypt)

--- src/routers/tasks.js
+++ src/routers/tasks.js
@@ -37,19 +37,19 @@ router.get('/tasks/:id', async (req, res) => {
 router.patch('/tasks/:id', async (req, res) => {
   const updates = Object.keys(req.body);
   const allowedUpdates = ['description', 'completed'];
-  const isValidUpdate = updates.every(update =>
+  const isValidOperation = updates.every(update =>
     allowedUpdates.includes(update)
   );
 
-  if (!isValidUpdate) {
+  if (!isValidOperation) {
     return res.status(400).send({ error: 'Invalid updates!' });
   }
 
   try {
-    const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true
-    });
+    const task = await Task.findById(req.params.id);
+
+    updates.forEach(update => (task[update] = req.body[update]));
+    await task.save();
 
     if (!task) {
       return res.status(404).send();
--- src/routers/users.js
+++ src/routers/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const User = require('../models/user');
+const bcrypt = require('bcryptjs');
 const router = new express.Router();
 
 router.post('/users', async (req, res) => {
@@ -47,10 +48,10 @@ router.patch('/users/:id', async (req, res) => {
   }
 
   try {
-    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true
-    });
+    const user = await User.findById(req.params.id);
+
+    updates.forEach((update) => user[update] = req.body[update]);
+    await user.save();
 
     if (!user) {
       return res.status(404).send();
