0e362e8029359593e5d88b686223d5cf09d86fef
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2019-10-08 23:52:02 +0530

    add logout and logoutAll

--- src/routers/users.js
+++ src/routers/users.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const User = require('../models/user');
 const bcrypt = require('bcryptjs');
+const auth = require('../middleware/auth');
 const router = new express.Router();
 
 router.post('/users', async (req, res) => {
   const user = new User(req.body);
-  
+
   try {
     await user.save();
     const token = await user.generateAuthToken();
@@ -30,14 +31,32 @@ router.post('/users/login', async (req, res) => {
   }
 });
 
-router.get('/users', async (req, res) => {
+router.post('/users/logout', auth, async (req, res) => {
   try {
-    const users = await User.find({});
+    req.user.tokens = req.user.tokens.filter(
+      token => token.token !== req.token
+    );
+    await req.user.save();
 
-    res.send(users);
+    res.send();
   } catch (e) {
-    res.status(500).send(e);
+    res.status(500).send();
   }
+});
+
+router.post('/users/logoutAll', auth, async (req, res) => {
+  try {
+    req.user.tokens = [];
+    await req.user.save();
+
+    res.send();
+  } catch (e) {
+    res.status(500).send();
+  }
+});
+
+router.get('/users/me', auth, async (req, res) => {
+  res.send(req.user);
 });
 
 router.get('/users/:id', async (req, res) => {
