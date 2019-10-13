78818c7b95c0e524d2efa41f2f9c90560298d19b
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2019-10-13 23:06:02 +0530

    added auth to routes and refactor routes for single user

--- src/routers/users.js
+++ src/routers/users.js
@@ -65,20 +65,7 @@ router.get('/users/me', auth, async (req, res) => {
 	res.send(req.user);
 });
 
-router.get('/users/:id', async (req, res) => {
-	try {
-		const _id = req.params.id;
-		const user = await User.findById(_id);
-		if (!user) {
-			return res.status(404);
-		}
-		res.send(user);
-	} catch (e) {
-		res.status(500).send(e);
-	}
-});
-
-router.patch('/users/:id', async (req, res) => {
+router.patch('/users/me', auth, async (req, res) => {
 	const updates = Object.keys(req.body);
 	const allowedUpdates = ['name', 'email', 'password', 'age'];
 	const isValidUpdate = updates.every(update =>
@@ -91,28 +78,20 @@ router.patch('/users/:id', async (req, res) => {
 	}
 
 	try {
-		const user = await User.findById(req.params.id);
+		updates.forEach(update => (req.user[update] = req.body[update]));
 
-		updates.forEach(update => (user[update] = req.body[update]));
-		await user.save();
+		await req.user.save();
 
-		if (!user) {
-			return res.status(404).send();
-		}
-		res.send(user);
+		res.send(req.user);
 	} catch (e) {
 		res.status(400).send(e);
 	}
 });
 
-router.delete('/users/:id', async (req, res) => {
+router.delete('/users/me', auth, async (req, res) => {
 	try {
-		const user = await User.findByIdAndDelete(req.params.id);
-
-		if (!user) {
-			return res.status(404).send();
-		}
-		res.send(user);
+		await req.user.remove();
+		res.send(req.user);
 	} catch (e) {
 		res.status(500).send(e);
 	}
